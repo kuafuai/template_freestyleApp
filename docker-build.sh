0ebec290d6b0d3ee762e9c35096c8f724637b1cc
booboosui <139328168+booboosui@users.noreply.github.com>
2023-08-15 11:33:47 +0800

    Create docker-build.sh

---
+++ docker-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get the current commit SHA
+COMMIT_SHA=$(git rev-parse HEAD)
+
+# Build the Docker image
+docker build -t your-docker-username/nginx-image:${COMMIT_SHA} -t your-docker-username/nginx-image:latest -f Dockerfile .
+
+# Login to Docker Hub
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+
+# Push the Docker image
+docker push your-docker-username/nginx-image:${COMMIT_SHA}
+docker push your-docker-username/nginx-image:latest
