ac52cd35138fd2bf52fb21de034406bee49c63e4
booboosui <139328168+booboosui@users.noreply.github.com>
2023-08-15 11:42:18 +0800

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -4,7 +4,7 @@
 COMMIT_SHA=$(git rev-parse HEAD)
 
 # Build the Docker image
-docker build -t your-docker-username/nginx-image:${COMMIT_SHA} -t your-docker-username/nginx-image:latest -f Dockerfile .
+docker build -t kuafuai/template-freestyleapp:${COMMIT_SHA} -t your-docker-username/nginx-image:latest -f Dockerfile .
 
 # Login to Docker Hub
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
