be8d710d755c064b31318e7f4ea414fe1114c5a7
booboosui <suixiaobo@kuafuai.net>
2023-09-11 12:56:42 +0800

    写一个网络爬虫，爬取今日头条的热门新闻标题

--- crawler.py
+++ crawler.py
@@ -2,22 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_hot_news_titles():
-    # Sends a GET request to the homepage of Today's Headlines
+    # Function to scrape the hot news titles from the homepage of Today's Headlines
     response = requests.get("https://www.toutiao.com/")
-    
-    # Parses the HTML content of the response
     soup = BeautifulSoup(response.content, "html.parser")
-    
-    # Locates the elements that contain the hot news titles
-    hot_news_elements = soup.select(".hotnews .title")
-    
-    # Extracts the content of the hot news titles elements
+    hot_news_elements = soup.select("CSS_SELECTOR_FOR_HOT_NEWS_TITLES")
     hot_news_titles = [element.text for element in hot_news_elements]
-    
     return hot_news_titles
 
-# Main part of the code
-if __name__ == "__main__":
-    hot_news_titles = get_hot_news_titles()
-    for title in hot_news_titles:
-        print(title)
+# Main code
+hot_news_titles = get_hot_news_titles()
+print(hot_news_titles)
