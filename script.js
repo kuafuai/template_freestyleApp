d839ca7e4f78366d57eddbe282542ed1af23bb7c
charging-kuafuai <charging@126.com>
2023-09-16 12:32:20 +0800

    开发一个可以在本地端运行的俄罗斯方块游戏

--- script.js
+++ script.js
@@ -9,54 +9,116 @@ let gameOver = false;
 
 // Function to handle user input
 function handleInput(event) {
-  // Code to handle user input
-  // Example implementation:
   if (event.key === "ArrowLeft") {
-    // Move the current block to the left
+    moveBlockLeft();
   } else if (event.key === "ArrowRight") {
-    // Move the current block to the right
+    moveBlockRight();
   } else if (event.key === "ArrowDown") {
-    // Move the current block down
+    moveBlockDown();
   } else if (event.key === "ArrowUp") {
-    // Rotate the current block
+    rotateBlock();
+  }
+}
+
+// Function to move the current block to the left
+function moveBlockLeft() {
+  const newBlock = currentBlock.map(row => row.slice(0, -1));
+  if (!isCollision(newBlock)) {
+    currentBlock = newBlock;
+  }
+}
+
+// Function to move the current block to the right
+function moveBlockRight() {
+  const newBlock = currentBlock.map(row => row.slice(1));
+  if (!isCollision(newBlock)) {
+    currentBlock = newBlock;
+  }
+}
+
+// Function to move the current block down
+function moveBlockDown() {
+  const newBlock = currentBlock.slice(1);
+  if (!isCollision(newBlock)) {
+    currentBlock = newBlock;
+  } else {
+    mergeBlock();
+    generateBlock();
+  }
+}
+
+// Function to rotate the current block
+function rotateBlock() {
+  const newBlock = transpose(currentBlock).map(row => row.reverse());
+  if (!isCollision(newBlock)) {
+    currentBlock = newBlock;
   }
 }
 
 // Function to generate random blocks
 function generateBlock() {
-  // Code to generate random blocks
-  // Example implementation:
   const blocks = ["I", "J", "L", "O", "S", "T", "Z"];
   const randomIndex = Math.floor(Math.random() * blocks.length);
   const randomBlock = blocks[randomIndex];
-  currentBlock = randomBlock;
+  currentBlock = getRandomBlock(randomBlock);
   return randomBlock;
+}
+
+// Function to get the block shape based on the block type
+function getRandomBlock(blockType) {
+  if (blockType === "I") {
+    return [[1, 1, 1, 1]];
+  } else if (blockType === "J") {
+    return [[1, 0, 0], [1, 1, 1]];
+  } else if (blockType === "L") {
+    return [[0, 0, 1], [1, 1, 1]];
+  } else if (blockType === "O") {
+    return [[1, 1], [1, 1]];
+  } else if (blockType === "S") {
+    return [[0, 1, 1], [1, 1, 0]];
+  } else if (blockType === "T") {
+    return [[0, 1, 0], [1, 1, 1]];
+  } else if (blockType === "Z") {
+    return [[1, 1, 0], [0, 1, 1]];
+  }
 }
 
 // Function to check if a row is full
 function isRowFull(row) {
-  // Code to check if a row is full
-  // Example implementation:
-  for (let i = 0; i < gameArea[row].length; i++) {
-    if (!gameArea[row][i]) {
-      return false;
-    }
-  }
-  return true;
+  return gameArea[row].every(cell => cell !== 0);
 }
 
 // Function to clear a full row
 function clearRow(row) {
-  // Code to clear a full row
-  // Example implementation:
   gameArea.splice(row, 1);
   gameArea.unshift(new Array(10).fill(0));
 }
 
+// Function to merge the current block with the game area
+function mergeBlock() {
+  for (let row = 0; row < currentBlock.length; row++) {
+    for (let col = 0; col < currentBlock[row].length; col++) {
+      if (currentBlock[row][col] !== 0) {
+        gameArea[row][col] = currentBlock[row][col];
+      }
+    }
+  }
+}
+
+// Function to check if there is a collision between the current block and the game area
+function isCollision(block) {
+  for (let row = 0; row < block.length; row++) {
+    for (let col = 0; col < block[row].length; col++) {
+      if (block[row][col] !== 0 && (gameArea[row] && gameArea[row][col] !== 0)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 // Function to update game state
 function updateGameState() {
-  // Code to update game state
-  // Example implementation:
   if (isRowFull(0)) {
     clearRow(0);
     score += 100;
@@ -72,14 +134,13 @@ document.addEventListener("keydown", handleInput);
 
 // Game loop
 function gameLoop() {
-  // Code for the game loop
-  // Example implementation:
   if (!gameOver) {
     updateGameState();
-    // Other game logic
+    moveBlockDown();
     setTimeout(gameLoop, 1000);
   }
 }
 
 // Start the game loop
+generateBlock();
 gameLoop();
