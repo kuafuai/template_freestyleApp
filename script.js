ea07039260e62b6e460111cc9d1239b495ed8cbd
charging-kuafuai <charging@126.com>
2023-10-11 14:29:37 +0800

    开发一个前端静态网站，网站内容如下：
    # 网页标题：
    北京烤鸭之美
    
    # 页面内容：
    1. 主页
    引人入胜的照片：一张美味的北京烤鸭图片。
    欢迎词：欢迎来到北京烤鸭之美，探索中国烹饪的精髓。
    提供了一个按钮链接到“品尝北京烤鸭”页面，以引导用户了解如何品尝这道美食。
    
    2. 关于北京烤鸭
    一张北京烤鸭的图片。
    简介：北京烤鸭是中国烹饪的杰出代表之一，它是一道美味的烤鸭菜肴，具有悠久的历史。
    重要特点：解释北京烤鸭之美的独特之处，如脆皮、嫩肉和独特的烹饪过程。
    
    3. 制作过程
    清晰的步骤：展示如何制作北京烤鸭的关键步骤，包括鸭的处理、腌制、烤制等。
    图片展示：每个步骤附上图片以使用户更容易理解。
    
    4. 品尝北京烤鸭
    探索北京烤鸭的不同吃法：包括将北京烤鸭卷成薄饼，搭配特制的酱料和蔬菜，以及其他传统吃法。
    推荐佐餐酒：介绍不同种类的酒或饮料，与北京烤鸭搭配，以提升食欲。
    
    5. 历史文化
    简要历史：关于北京烤鸭的历史和文化背景。
    相关故事：包括与北京烤鸭相关的传统故事和文化元素。
    
    6. 联系我们
    联系信息：提供联系方式，包括地址、电话号码和电子邮件地址，以便用户获取更多信息或预订北京烤鸭。
    反馈表单：用户可以通过反馈表单提供意见和建议。
    
    页脚：
    版权信息
    链接到社交媒体页面
    隐私政策和条款与条件链接

---
+++ script.js
@@ -0,0 +1,5 @@
+// Handle button click event to navigate to "品尝北京烤鸭" page
+document.querySelector("#home a").addEventListener("click", function(event) {
+    event.preventDefault();
+    window.location.href = "#taste";
+});
