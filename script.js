54ba0734a92ae6d0aee840df5bf566040270dd92
charging-kuafuai <charging@126.com>
2023-10-17 09:30:11 +0800

    产品信息列表 对应 缓存信息菜单，通过选择产品信息来进入缓存管理页面。 缓存管理：支持添加新的缓存和编辑，包括参数字段，响应状态，创建时间，更新时间。还需一个缓存详情页面 缓存详情页面：是一个表格，内容有 请求时间，响应状态，上游信息，请求ID。

---
+++ script.js
@@ -0,0 +1,57 @@
+// script.js
+
+// Function to get product information list from the backend
+function getProductInformationList() {
+    // Call backend API to get product information list
+    // Implementation details
+}
+
+// Function to add new cache using backend API
+function addNewCache() {
+    // Call backend API to add new cache
+    // Implementation details
+}
+
+// Function to edit cache using backend API
+function editCache() {
+    // Call backend API to edit cache
+    // Implementation details
+}
+
+// Function to get cache information (parameters, status, creation time, update time) from the backend
+function getCacheInformation() {
+    // Call backend API to get cache information
+    // Implementation details
+}
+
+// Function to get cache details (request time, status, upstream information, request ID) from the backend
+function getCacheDetails() {
+    // Call backend API to get cache details
+    // Implementation details
+}
+
+// Function to display product information list in the menu
+function displayProductInformationList() {
+    // Get product information list from the backend
+    // Implementation details
+    // Display product information list in the menu
+}
+
+// Function to display cache information (parameters, status, creation time, update time) in the cache management page
+function displayCacheInformation() {
+    // Get cache information from the backend
+    // Implementation details
+    // Display cache information in the cache management page
+}
+
+// Function to display cache details (request time, status, upstream information, request ID) in the cache details page
+function displayCacheDetails() {
+    // Get cache details from the backend
+    // Implementation details
+    // Display cache details in the cache details page
+}
+
+// Call necessary functions to initialize the cache management page
+displayProductInformationList();
+displayCacheInformation();
+displayCacheDetails();
