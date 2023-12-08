e1f2a838d9bb2ab313793c47b77727f6e1e7e8ba
charging-kuafuai <charging@126.com>
2023-12-08 16:05:20 +0800

    企业信息门户：包括首页、产品介绍、资讯动态、公司介绍等功能

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 // COMMENT
 // Add event listeners or implement any necessary JavaScript functionality
+
+// Add event listener to a button with id "myButton"
+document.getElementById("myButton").addEventListener("click", function() {
+  // Functionality to be implemented when the button is clicked
+  console.log("Button clicked!");
+});
+
+// Add event listener to an input field with id "myInput"
+document.getElementById("myInput").addEventListener("input", function() {
+  // Functionality to be implemented when the input field value changes
+  console.log("Input value changed!");
+});
+
+// Add event listener to a form with id "myForm"
+document.getElementById("myForm").addEventListener("submit", function(event) {
+  // Prevent the form from submitting
+  event.preventDefault();
+  
+  // Functionality to be implemented when the form is submitted
+  console.log("Form submitted!");
+});
