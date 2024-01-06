0e1d3a318c051a4e8ad7735ae0e28a6d69545caa
charging-kuafuai <charging@126.com>
2024-01-06 16:53:06 +0800

    - 建议1: 在需求分析中，可以进一步明确通用零部件测试设备和程序的信息内容，例如设备的规格、参数、状态等，程序的功能、支持的操作系统等。 - 建议2: 在UI设计中，可以考虑添加设备和程序的图片展示功能，以便用户更直观地了解设备和程序的外观和界面。 - 建议3: 在UI设计中，可以为设备和程序列表添加分页功能，以便在数据量较大时能够更好地浏览和管理。

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+# Import necessary modules
 from flask_sqlalchemy import SQLAlchemy
 
 # Create database object
@@ -42,44 +43,11 @@ def delete_data(data):
     db.session.delete(data)
     db.session.commit()
 
-# Update data in the database
-def update_data(data_id, new_data):
-    data = get_data(Data, data_id)
-    if data:
-        data.name = new_data['name']
-        db.session.commit()
+def update_data():
+    db.session.commit()
 
-# Get data from the database
 def get_data(model, id):
     return model.query.get(id)
 
-# Get all data from the database
 def get_all_data(model):
     return model.query.all()
-
-# Search data in the database based on keyword
-def search_data(keyword):
-    return [{'id': data.id, 'name': data.name} for data in db.session.query(Data).filter(Data.name.like(f'%{keyword}%')).all()]
-
-# Initialize database
-Base.metadata.create_all(engine)
-
-# Define database class
-class Database:
-    def __init__(self):
-        self.session = Session()
-
-    def add_data(self, data):
-        add_data(data)
-
-    def delete_data(self, data_id):
-        delete_data(get_data(Data, data_id))
-
-    def update_data(self, data_id, new_data):
-        update_data(data_id, new_data)
-
-    def get_data(self, data_id):
-        return get_data(Data, data_id)
-
-    def search_data(self, keyword):
-        return search_data(keyword)
