de31ed6a25463ed682316eb89a78d3822b9a6346
charging-kuafuai <charging@126.com>
2024-05-28 15:32:35 +0800

    开发一个HTML页面，主要用户旅游的展示和推荐，包括以下几个方面的内容： 1、首页主要为宣传磐安旅游的视频和图片，在首页下面有两个入口，分别为“旅游线路定制”和“智慧导览” 2、旅游线路定制用于用户根据自己的出行人员的情况，结合各个景点的特点，给客户推荐一个旅游的出行计划和路线，并且可以向客户推荐公交线路以及包车服务 3、智慧导览：用于客户在具体的景点，根据用户的定位来触发讲解内容，让游客能够更加生动的了解景点的历史背景、故事等内容

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
 function goToCustomization() {
-    window.location.href = "customization.html";
+    if (pageExists("customization.html")) {
+        window.location.href = "customization.html";
+    } else {
+        displayErrorMessage("The customization page does not exist.");
+    }
 }
 
 function goToGuide() {
-    window.location.href = "guide.html";
+    if (pageExists("guide.html")) {
+        window.location.href = "guide.html";
+    } else {
+        displayErrorMessage("The guide page does not exist.");
+    }
+}
+
+function pageExists(page) {
+    // Check if the page exists by making a HEAD request
+    var xhr = new XMLHttpRequest();
+    xhr.open("HEAD", page, false);
+    xhr.send();
+    return xhr.status != 404;
+}
+
+function displayErrorMessage(message) {
+    // Display the error message to the user
+    alert(message);
 }
