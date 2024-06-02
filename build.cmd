d51b053feacfaa1b4ef8e6b2579e1b29553497d8
charging-kuafuai <charging@126.com>
2024-06-02 20:26:31 +0800

    个卖衣服的网站，可以看到图片，点击图片可以看到详细信息

--- build.cmd
+++ build.cmd
@@ -1,6 +1,5 @@
-@echo off
-
 REM build.cmd
 REM This file is responsible for displaying a message instructing the user to complete the compilation script "./build.sh".
 
+@echo off
 echo Please complete the compilation script "./build.sh".
--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
 # build.sh
 # This file is responsible for displaying a message instructing the user to complete the compilation script.
-
-#echo "Please complete the compilation script ./build.sh"
 
 echo "Please complete the compilation script."
